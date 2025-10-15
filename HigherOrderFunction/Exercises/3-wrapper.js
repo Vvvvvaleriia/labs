f762343cc4d3c3ee6c8290740e0bce0c0bb7b6b4
Valeriia <reznikvaleria31@gmail.com>
2025-10-15 20:24:43 +0300

    Add files via upload

---
+++ HigherOrderFunction/Exercises/3-wrapper.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const contract = (fn, ...types) => (...args) =>{
+    for (let i = 0; i < args.length; i++){
+        const arg = args[i];
+        const def = types[i];
+        const name = def.name.toLowerCase();
+        if (typeof arg !== name){
+            throw new TypeError('Argument name ${name} expected');
+        }
+    }
+    const res = fn(...args);
+    const def = types[types.length - 1];
+    const name = def.name.toLowerCase();
+    if (typeof res !== name){
+        throw new TypeError('Result type ${name} expected');
+    }
+    return res;
+}
+
+const add = (a, b) => a + b;
+const addNumbers = contract(add, Number, Number, Number);
+const res = addNumbers(2, 3);
+console.dir(res); 
+module.exports = { contract };
